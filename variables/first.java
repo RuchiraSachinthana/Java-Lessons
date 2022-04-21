cc7f938cdf110d42616c71c805f0d000b8fa7838
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2022-04-21 22:59:42 +0530

    updated

--- variables/first.java
+++ variables/first.java
@@ -2,9 +2,26 @@ package variables;
 
 public class first {
 
+   static int myNum = 5;               // Integer (whole number)
+   static float myFloatNum = 5.99f;    // Floating point number
+   static char myLetter = 'D';         // Character
+   static boolean myBool = true;       // Boolean
+   static String myText = "Hello";     // String
     public static void main(String[] args) {
        int number = 10;
 
         System.out.println(number);
+
+        int Age = 18;
+        String Name = "JavaC";
+
+        System.out.print("Your age is: ");
+        System.out.println(Age);
+        System.out.print("Welcome: ");
+        System.out.println(Name);
+
+
+
+
     }
 }
